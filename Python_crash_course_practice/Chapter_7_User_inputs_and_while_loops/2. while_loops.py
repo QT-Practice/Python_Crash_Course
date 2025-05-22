b209c347987d0f1f6bb44467a198a04b5cff6ed9
srikvp <93551165+srikvp@users.noreply.github.com>
2025-05-22 18:50:06 -0400

    fixed syntax

--- Python_crash_course_practice/Chapter_7_User_inputs_and_while_loops/2. while_loops.py
+++ Python_crash_course_practice/Chapter_7_User_inputs_and_while_loops/2. while_loops.py
@@ -157,4 +157,4 @@ while polling_active:
 # Polling is complete. Show the results.
 print("\n--- Poll Results ---")
 for name, response in responses.items():
-print(f"{name} would like to climb {response}.")
+    print (f"{name} would like to climb {response}.")
