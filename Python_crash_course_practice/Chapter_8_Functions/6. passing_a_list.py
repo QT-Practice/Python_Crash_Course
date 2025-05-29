90209349f663d3dc875f592a56d27d2a917f61fe
srikvp <93551165+srikvp@users.noreply.github.com>
2025-05-29 13:43:58 -0400

    modified Chapter_8_Functions

--- Python_crash_course_practice/Chapter_8_Functions/6. passing_a_list.py
+++ Python_crash_course_practice/Chapter_8_Functions/6. passing_a_list.py
@@ -30,3 +30,26 @@ while unprinted_designs:
 print("\nThe following models have been printed:")
 for completed_model in completed_models:
     print(completed_model)
+
+# reoganizing the above code by writing two functions
+# first function handles printing the designs
+# second function summarizes the prints that have been made
+
+def print_models(unprinted_designs, completed_models):
+    """Simulate printing each design, until none are left.
+       Move each design to completed_models after printing."""
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
